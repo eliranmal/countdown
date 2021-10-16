ec7c1796eef77adcf1074ae0ac06e23cfa481af8
eliran <eliranmal@gmail.com>
2021-10-17 02:46:02 +0300

    refactor(utils): add default assignments

--- src/lib/object.js
+++ src/lib/object.js
@@ -1,6 +1,6 @@
 
 // mutates obj!
-export const patch = (obj, newObj) => Object.assign(obj, newObj)
+export const patch = (obj = {}, newObj = {}) => Object.assign(obj, newObj)
 
 export const serialize = (...args) => args.length ? JSON.stringify(...args) : ''
 
