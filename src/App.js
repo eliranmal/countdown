604dd38cb034e4e65e68acac9865e1b9c515e724
eliran <eliranmal@gmail.com>
2021-10-17 15:24:22 +0300

    fix: update timer state properly into local storage

--- src/App.js
+++ src/App.js
@@ -1,59 +1,48 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect} from 'react'
+import useLocalStorage from 'use-local-storage'
 import {loadObject, saveObject} from './lib/storage'
-import {serialize} from './lib/object'
 import timer from './lib/timer'
-import './App.css';
-
-const timerConfig = {
-  direction: 'down',
-}
-let countdownTimer = timer(timerConfig)
+import './App.css'
 
 function App() {
 
-  const [ellapsedTime, setEllapsedTime] = useState(countdownTimer.getEllapsedTimeString())
-  const [state, setState] = useState(countdownTimer.getState())
+  const [timerState, setTimerState] = useLocalStorage('timer-state', {})
+  const [timerEvents, setTimerEvents] = useLocalStorage('timer-events', [])
+
+  const countdownTimer = timer({
+    direction: 'down',
+  }, timerState, timerEvents)
+
   const [laps, setLaps] = useState(countdownTimer.getLaps())
+  const [ellapsedTime, setEllapsedTime] = useState(countdownTimer.getEllapsedTimeString())
 
-  useEffect(() => {
-    // load application state from storage, in case the tab was closed/refreshed
-    const appState = loadObject('countdown-state')
-    setState(appState.state)
-    setLaps(appState.laps)
-    countdownTimer = timer(timerConfig, appState.state, appState.events)
-    console.log('countdownTimer', countdownTimer)
-  }, [])
+  const patchTimerData = () => {
+    setTimerState({...timerState, ...countdownTimer.getState()})
+    setTimerEvents([...countdownTimer.getEvents()])
+  }
 
-  useEffect(() => {
-    // when the tab is closed/refreshed, save application state to storage
-    const unloadListener = saveObject.bind(null, 'countdown-state', {
-      state: countdownTimer.getState(),
-      events: countdownTimer.getEvents(),
-      laps: countdownTimer.getLaps(),
-    })
-    window.addEventListener('beforeunload', unloadListener)
-    return () => {
-      window.removeEventListener('beforeunload', unloadListener)
-    }
-  }, [])
+  const setTimerData = () => {
+    setTimerState({...countdownTimer.getState()})
+    setTimerEvents([...countdownTimer.getEvents()])
+  }
+
+  const renderButton = (command, callback = patchTimerData) => (<button
+    title={command}
+    className={`App-button-${command}`}
+    onMouseDown={() => countdownTimer[command]() && callback()}
+  ></button>)
 
   useEffect(() => {
     let reqId = window.requestAnimationFrame(function step() {
       setEllapsedTime(countdownTimer.getEllapsedTimeString())
-      if (state.running && !state.paused) {
+      if (timerState.running && !timerState.paused) {
         reqId = window.requestAnimationFrame(step)
       }
     })
     return () => {
       window.cancelAnimationFrame(reqId)
     }
-  }, [state])
-
-  const renderFlippingButton = command => (<button
-    title={command}
-    className={`App-button-${command}`}
-    onMouseDown={() => countdownTimer[command]() && setState({...state, ...countdownTimer.getState()})}
-  ></button>)
+  }, [timerState.running, timerState.paused])
 
   return (
     <div className="App">
@@ -62,19 +51,19 @@ function App() {
       </header>
       <main className="App-main">
         <code>
-          {(countdownTimer.getLaps() || []).map(lapData => <p>{lapData}</p>)}
+          {(countdownTimer.getLaps() || [])
+            .map((lapData, key) => <p key={key}>{lapData}</p>)}
         </code>
-        <pre className="App-time-counter">
+        <pre className="App-time-display">
           {ellapsedTime}
         </pre>
         <nav className="App-controls">
-
           {/* todo - bind keyboard events */}
           {/* todo - don't setState, do it more selectively. what do we really need here? */}
-          {renderFlippingButton(state.running ? state.paused ? 'resume' : 'pause' : 'start')}
-          <button title="stop" className="App-button-stop" onMouseDown={() => countdownTimer.stop() && setState({...state, ...countdownTimer.getState()})}></button>
-          <button title="lap" className="App-button-lap" onMouseDown={() => countdownTimer.lap() && setState({...state, ...countdownTimer.getState()})}></button>
-          <button title="clear" className="App-button-clear" onMouseDown={() => countdownTimer.clear() && setState({...state, ...countdownTimer.getState()})}></button>
+          {renderButton(timerState.running ? timerState.paused ? 'resume' : 'pause' : 'start')}
+          {renderButton('stop')}
+          {renderButton('lap')}
+          {renderButton('clear', setTimerData)}
         </nav>
       </main>
     </div>
--- src/lib/timer.js
+++ src/lib/timer.js
@@ -1,14 +1,11 @@
 import {patch} from './object'
 
-// the result of: new Date(1970, 0, 1).getTime()
-const EPOCH = -7200000
-
 // todo - optionally accept a storage object with known crud api, and compse it
 const init = ({
   direction = 'up',
   duration = 30 * 1000,
   threshold = 10 * 1000,
-} = {}, stateSnapshot = {}, eventsSnapshot) => {
+} = {}, stateSnapshot, eventsSnapshot) => {
 
   let laps = []
   let state = stateSnapshot ?? {}
@@ -19,23 +16,26 @@ const init = ({
   // }
   let events = eventsSnapshot ?? []
 
-  const setState = newState => patch(state, newState)
-  const setLaps = newLaps => patch(laps, newLaps)
+  const setState = newState => (state = newState)
+  const patchState = newState => patch(state, newState)
+  const setLaps = newLaps => (laps = newLaps)
+  const setEvents = newEvents => (events = newEvents)
 
-  const resolveLaps = (state, lapTimestamps) =>
-  (lapTimestamps || [])
-  .map(time => time - state.startTime)
+  const resolveLaps = (lapTimestamps = []) => lapTimestamps
+    .map(time => time)
   // todo - use reduce to compare prev time and curr time, and add that as 'duration'
 
   // ----------------------------------------------------------------
   // todo - extract method (see below) and continue from there
-  .filter(({type}) => type === 'lap')
-  .map(val => val)
+  // .filter(({type}) => type === 'lap')
+  // .map(val => val)
 
 
-  const getEvents = () => events
-  const getState = () => state
-  const getLaps = () => resolveLaps(state, events)
+  const getEvents = () => events ?? []
+  const getState = () => state ?? {}
+  const getLaps = () => resolveLaps(events
+    .filter(({type}) => type === 'lap')
+    .map(({timestamp}) => timestamp))
 
   const _getEllapsedTime = () => events
   // todo - extract some of this to a utilty that transforms the absolute data (global timestamps) to relative data (the timer period sums), then share with resolveLaps()
@@ -70,17 +70,22 @@ const init = ({
     timestamp: time,
   })
 
-  const start = command('start',
-  time => !state.running && setState({running: true, startTime: time}))
-  const pause = command('pause',
-  () => state.running && !state.paused && setState({paused: true}))
-  const resume = command('resume',
-  () => state.running && state.paused && setState({paused: false}))
-  const stop = command('stop',
-  time => state.running && setState({endTime: time, running: false}))
-  const lap = command('lap',
-  time => state.running && !state.paused && setLaps([...laps, time]))
-  const clear = () => !state.running && (state = {}) && (events = [])
+  const start = command(
+    'start',
+    time => !state.running && patchState({running: true}))
+  const pause = command(
+    'pause',
+    () => state.running && !state.paused && patchState({paused: true}))
+  const resume = command(
+    'resume',
+    () => state.paused && state.paused && patchState({paused: false}))
+  const stop = command(
+    'stop',
+    time => state.running && patchState({running: false}))
+  const lap = command(
+    'lap',
+    time => state.running && !state.paused && laps.push(time))
+  const clear = () => !state.running && setLaps([]) && setState({}) && setEvents([])
 
   return {
     start,
