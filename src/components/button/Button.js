123e2f5b15aba5e4a1b6ddd4f2c9721ba9880a51
eliran <eliranmal@gmail.com>
2021-10-22 01:31:05 +0300

    improve: align button icons

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -4,22 +4,24 @@ import Icon from '../icon/Icon'
 import './Button.css'
 
 const baseIconProps = {
-  size: '45%',
+  size: '50%',
   color: '#ccc',
 }
 
 const iconPropMap = {
   play: { ...baseIconProps, style: {
-      marginRight: '-6%',
+    marginRight: '-6%',
+    marginLeft: '6%',
   }},
-  stop: baseIconProps,
+  stop: { ...baseIconProps, size: '45%' },
   pause: baseIconProps,
   eject: { ...baseIconProps, style: {
     marginRight: '-6%',
     transform: 'rotate(90deg)',
   }},
-  rotate: { ...baseIconProps, size: '60%', style: {
-    marginRight: '-6%',
+  rotate: { ...baseIconProps, size: '65%', style: {
+    marginRight: '-4%',
+    marginLeft: '4%',
     transform: 'rotate(90deg)',
   }},
   io: { ...baseIconProps, size: '60%', style: {
