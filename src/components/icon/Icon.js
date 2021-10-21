749f51802f3357e3204f6a6a4b59d1bc00455bf1
eliran <eliranmal@gmail.com>
2021-10-21 19:39:52 +0300

    refactor: encapsulation

--- src/components/icon/Icon.js
+++ src/components/icon/Icon.js
@@ -1,27 +1,23 @@
-import React, {useRef, useState, useEffect} from 'react'
-import {iterator as colors} from '../../lib/colors'
+import React from 'react'
 import { ReactSVG } from 'react-svg'
+import {iterator as colors} from '../../lib/colors'
 
 import './Icon.css'
 
+// true value signals that the icon color should be chosen internally
+const resolveColor = color => color === true ?
+  colors.next() :
+    color || 'var(--redhat)'
 
-const Icon = ({ name, color, size = '24px', ...props }) => {
-
-  // true value signals that the icon color should be chosen internally
-  const iconColor = color === true ?
-    colors.next() :
-      color || 'var(--redhat)'
-
-  return <ReactSVG
+const Icon = ({ name, color, size = '24px', ...props }) => (<ReactSVG
     src={require(`../../images/icons/${name}.svg`).default}
     className="cd-icon"
     style={{
       '--icon-size': size,
-      '--icon-color': iconColor,
+      '--icon-color': resolveColor(color),
       ...props.style
     }}
-  />
-}
+  />)
 
 
 export default Icon
