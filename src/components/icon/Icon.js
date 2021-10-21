6435815bedf991a2b677877965b8805c1abc6ac6
eliran <eliranmal@gmail.com>
2021-10-21 19:23:47 +0300

    fix: dynamic importing of svg
    - add react-svg lib, to resolve loading svg in production
    - update icon component implementation to match
    - deploy and cross fingers

--- src/components/icon/Icon.js
+++ src/components/icon/Icon.js
@@ -1,39 +1,26 @@
 import React, {useRef, useState, useEffect} from 'react'
 import {iterator as colors} from '../../lib/colors'
+import { ReactSVG } from 'react-svg'
+
 import './Icon.css'
 
 
 const Icon = ({ name, color, size = '24px', ...props }) => {
-  const IconRef = useRef(null)
-  const [loading, setLoading] = useState(false)
 
-  useEffect(() => {
-    (async () => (
-      await import(`!!@svgr/webpack?-svgo,+titleProp,+ref!../../images/icons/${name}.svg`)
-    ).default)
-    .call(null, setLoading(true))
-    .then(iconComponent => (IconRef.current = iconComponent))
-    .finally(() => setLoading(false))
-  }, [name])
-
-  if (!loading && IconRef.current) {
-    // true value signals that the icon color should be choosed internally
-    const iconColor = color === true ?
-      colors.next() :
+  // true value signals that the icon color should be chosen internally
+  const iconColor = color === true ?
+    colors.next() :
       color || 'var(--redhat)'
-    const { current: IconComponent } = IconRef
 
-    return <IconComponent
-      className="cd-icon"
-      width={size}
-      height={size}
-      stroke={iconColor}
-      fill={iconColor}
-      {...props}
-    />
-  }
-
-  return <>&nbsp;</>;
+  return <ReactSVG
+    src={require(`../../images/icons/${name}.svg`).default}
+    className="cd-icon"
+    style={{
+      '--icon-size': size,
+      '--icon-color': iconColor,
+      ...props.style
+    }}
+  />
 }
 
 
