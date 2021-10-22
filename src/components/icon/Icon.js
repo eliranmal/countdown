49331d344fd548c44091165b1d7c1107125a9a36
eliran <eliranmal@gmail.com>
2021-10-22 18:08:25 +0300

    feat(components): extend icon api
    allow passing a custom className, and set default one

--- src/components/icon/Icon.js
+++ src/components/icon/Icon.js
@@ -11,9 +11,9 @@ const resolveColor = color => color === true ?
   colors.next().value :
     color || 'var(--redhat)'
 
-const Icon = ({ name, color, size = '24px', style = {} }) => (<ReactSVG
+const Icon = ({ name, color, size = '24px', style = {}, className }) => (<ReactSVG
     src={require(`../../images/icons/${name}.svg`).default}
-    className="cd-icon"
+    className={`cd-icon ${className}`}
     style={{
       '--icon-size': size,
       '--icon-color': resolveColor(color),
